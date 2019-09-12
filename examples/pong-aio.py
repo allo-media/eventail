f30cac24f0c62b39989b9541df067e6763d7b399
Romuald Texier-Marcadé <r.texiermarcade@allo-media.fr>
2019-09-12 17:06:54 +0200

    Initialized examples folder.

---
+++ examples/pong-aio.py
@@ -0,0 +1,44 @@
+import asyncio
+import sys
+
+import uvloop
+from async_service.aio import Service
+
+
+class EchoService(Service):
+
+    PREFETCH_COUNT = 10
+
+    async def handle_command(self, command, message, return_to, correlation_id):
+        await self.log("debug", "Received {}".format(command))
+        if command.split(".")[-1] == 'EchoMessage':
+            await self.log("info", "Echoing {}".format(message))
+            try:
+                await self.return_success(return_to, message, correlation_id, mandatory=True)
+            except ValueError:
+                await self.log("Error", f"Unroutable {return_to}")
+        else:
+            # should never happens: means we misconfigured the routing keys
+            await self.log("error", "unexpected message {}".format(command))
+            await self.return_error(
+                return_to,
+                {"reason": "unknown command", "message": "unknown {}".format(command)},
+                correlation_id)
+
+    async def heartbeat(self) -> None:
+        while True:
+            await self.log("health", "I'm fine!")
+            await asyncio.sleep(60)
+
+    async def on_ready(self) -> None:
+        self.create_task(self.heartbeat())
+
+
+if __name__ == '__main__':
+    uvloop.install()
+    url = sys.argv[1] if len(sys.argv) > 1 else "amqp://localhost"
+
+    loop = asyncio.get_event_loop()
+    service = EchoService(url, [], ["pong.EchoMessage"], "pong", loop=loop)
+    print('To exit press CTRL+C')
+    loop.run_until_complete(service.run())
---
+++ examples/pong-pika.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+
+from async_service.pika import Service, ReconnectingSupervisor
+
+
+class EchoService(Service):
+
+    PREFETCH_COUNT = 10
+
+    def handle_command(self, command, message, return_to, correlation_id):
+        self.log("debug", "Received {}".format(command))
+        if command.split(".")[-1] == 'EchoMessage':
+            self.log("info", "Echoing {}".format(message))
+            self.return_success(return_to, message, correlation_id)
+        else:
+            # should never happens: means we misconfigured the routing keys
+            self.log("error", "unexpected message {}".format(command))
+            self.return_error(
+                return_to,
+                {"reason": "unknown command", "message": "unknown {}".format(command)},
+                correlation_id)
+
+    def handle_returned_message(self, key, message, envelope):
+        self.log("error", "unroutable {}.{}.{}".format(
+            key, message, envelope))
+
+
+if __name__ == '__main__':
+    # import logging
+    # logger = logging.getLogger("async_service")
+    # logger.addHandler(logging.StreamHandler())
+    # logger.setLevel(logging.DEBUG)
+    url = sys.argv[1] if len(sys.argv) > 1 else "amqp://localhost"
+    echo = ReconnectingSupervisor(EchoService, url, [], ["pong.EchoMessage"], "pong")
+    print('To exit press CTRL+C')
+    echo.run()
+    print("Bye!")
