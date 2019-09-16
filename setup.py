c11e6ef0374a4f234185ebaee3d1d61919ae816c
Romuald Texier-Marcadé <r.texiermarcade@allo-media.fr>
2019-09-16 13:21:12 +0200

    New release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='async_service',
-    version='0.8',
+    version='0.9',
     url='https://github.com/allo-media/async-service',
     author='Allo-Media',
     author_email='dev@allo-media.fr',
