a2aecdf263a73305b13f23dc53cf367be6692854
Romuald Texier-Marcadé <r.texiermarcade@allo-media.fr>
2019-12-05 11:04:14 +0100

    Next version is 1.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='eventail',
-    version='1.0.4',
+    version='1.0.5',
     url='https://github.com/allo-media/async-service',
     author='Allo-Media',
     author_email='dev@allo-media.fr',
