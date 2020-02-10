5714b9280c8768639b1fb0e7defa735720df39a1
Romuald Texier-Marcadé <r.texiermarcade@allo-media.fr>
2020-02-10 17:15:50 +0100

    Simplified pong-pika example + new result paylod + uppcase echo.

--- examples/pong-pika.py
+++ examples/pong-pika.py
@@ -33,19 +33,15 @@ class EchoService(Service):
     PREFETCH_COUNT = 10
     RETRY_DELAY = 2
 
-    def on_EchoMessage(self, message, conversation_id, reply_to, correlation_id):
-        assert "message" in message, "missing 'message' key!"
+    def on_EchoMessage(self, payload, conversation_id, reply_to, correlation_id):
+        text = payload["message"].upper()
         self.log(
             INFO,
             "Echoing",
-            "Sending back {}".format(message),
+            "Sending back {}".format(text),
             conversation_id=conversation_id,
         )
-        self.return_success(reply_to, message, conversation_id, correlation_id)
-
-    def on_ShutdownStarted(self, payload, conversation_id):
-        self.log(INFO, "Received signal for shutdown.")
-        self.stop()
+        self.return_success(reply_to, {"echo": text}, conversation_id, correlation_id)
 
     def handle_returned_message(self, key, message, envelope):
         self.log(ERROR, "unroutable message", "{}.{}.{}".format(key, message, envelope))
@@ -59,13 +55,14 @@ class EchoService(Service):
 
 
 if __name__ == "__main__":
-    # import logging
-    # logger = logging.getLogger("async_service")
-    # logger.addHandler(logging.StreamHandler())
-    # logger.setLevel(logging.DEBUG)
+    import logging
+
+    logger = logging.getLogger("async_service")
+    logger.addHandler(logging.StreamHandler())
+    logger.setLevel(logging.DEBUG)
     urls = sys.argv[1:] if len(sys.argv) > 1 else ["amqp://localhost"]
     echo = ReconnectingSupervisor(
-        EchoService, urls, ["ShutdownStarted"], ["pong.EchoMessage"], "pong"
+        EchoService, urls, [], ["pong.EchoMessage"], "pong"
     )
     print("To exit press CTRL+C")
     echo.run()
